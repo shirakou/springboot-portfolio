630d353bb42aa9bc8dfa321d66e6baf3822b3d59
shirakou <koudai126126@icloud.com>
2026-06-25 22:42:37 +0900

    2026/06/25 の進捗

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -16,55 +16,60 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	//セキュリティの対象外を設定
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		//セキュリティー対象外の設定
-		http
-			.authorizeHttpRequests(authorize -> authorize
-					.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).
-permitAll()
-					.requestMatchers("/login").permitAll()
-					.requestMatchers("/user/signup").permitAll()
-					.requestMatchers("/error").permitAll()
-					.requestMatchers("/h2-console/**").permitAll()
-					.anyRequest().authenticated()
-			).formLogin(login -> login
-					.loginPage("/login")
-					.usernameParameter("userId")
-					.passwordParameter("password")
-					.defaultSuccessUrl("/user/list")
-					.failureUrl("/login?error")
-					.permitAll()
-			).logout(logout -> logout
-					.logoutUrl("/logout")
-					.logoutSuccessUrl("/login?logout")
-					);
-		
-		return http.build();
-	}
-	
-	//@Bean
-	UserDetailsService userDetailsService() {
-		//一般ユーザー
-		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("GENERAL")
-				.build();
-		//Adminnistrator
-		UserDetails admin = User.withDefaultPasswordEncoder()
-				.username("admin")
-				.password("password")
-				.roles("GENERAL","ADMIN")
-				.build();
-		
-		return new InMemoryUserDetailsManager(user,admin);
-	}
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /** セキュリティの対象外を設定 */
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // セキュリティ対象外の設定
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/user/signup").permitAll()
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .usernameParameter("userId")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/user/list")
+                        .failureUrl("/login?error")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout"))
+                .rememberMe(remember -> remember
+                        .rememberMeParameter("remember-me")
+                        .tokenValiditySeconds(3600));
+
+        // CSRFを無効（一時的）
+        // http.csrf(csrf -> csrf.disable());
+        // ヘッダー設定
+        // http.headers(headers -> headers.frameOptions(option -> option.disable()));
+
+        return http.build();
+    }
+
+    //@Bean
+    UserDetailsService userDetailsService() {
+        // 一般ユーザー
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("GENERAL")
+                .build();
+        // Administratorユーザー
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("password")
+                .roles("GENERAL", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
